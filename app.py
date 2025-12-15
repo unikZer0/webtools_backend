39067da64181bb773d52f75c0bf2240e18edcec9
unikZer0 <unik09john@gmail.com>
2025-12-15 14:18:44 +0700

    deploy

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
